0e02573f9bc3cd5c37d4975675c04f2fa19797e0
Anya Michaelsen <anya.michaelsen2@gmail.com>
2022-05-10 22:10:38 +0000

    Initial testing/package setup. Pytest not currently running

--- collegetools/generate_major_stats.py
+++ collegetools/generate_major_stats.py
@@ -8,7 +8,8 @@ years = [10,12,14,16,18]
 
 def compute_major_stats(df):
 	"""
-	Given a dataframe of entries for a specific major
+	Given a dataframe of entries for a specific major, return statistics for the group (see output).
+	Can be used in conjunction with pandas .groupby(*). 
 	
 	Input:
 	-----
@@ -55,7 +56,19 @@ def compute_major_stats(df):
 
 def compute_recent_grad_stats_by_major(years=years):
 	"""
-	DOCSTRING
+	Compute statistics on for each major over the years provided. 
+	
+	Requires: 
+	--------
+	data/fivethirtyeight/majors-list.csv     
+		- file containing FOD1P codes, names, and categories for each major 
+	data/pums/processed/XX_edu_wage_data.csv 
+		- file for year 20XX containing FOD1P and the features used in `compute_major_stats`
+	
+	Output:
+	-------
+	data/pums/processed/20XX_recent_grad_stats_by_major.csv 
+		- file for year 20XX with aggregated statistics per major on gender/wages
 	"""
 	major_list = pd.read_csv("data/fivethirtyeight/majors-list.csv")
 	major_list = major_list[~major_list['FOD1P'].str.contains('bb')]
