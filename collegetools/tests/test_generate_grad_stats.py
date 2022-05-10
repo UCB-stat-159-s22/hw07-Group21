20cff6ec585a5442420361c3aebfc7d81ec26a94
Anya Michaelsen <anya.michaelsen2@gmail.com>
2022-05-10 22:41:45 +0000

    Fixed pytest modulenotfound by adding pytest to env.yml

--- collegetools/tests/test_generate_grad_stats.py
+++ collegetools/tests/test_generate_grad_stats.py
@@ -1,15 +1,13 @@
 import pandas as pd
 import numpy as np
 
-# from collegetools import weighted_median
+from collegetools.generate_grad_stats import weighted_median
 
-def test_true():
-	assert True
-# def test_empty_median():
-# 	empty_df = pd.DataFrame()
-# 	assert weighted_median(empty_df) == -1 
+def test_empty_median():
+	empty_df = pd.DataFrame(columns=['val','wt'])
+	assert weighted_median(empty_df,'val','wt') == -1 
 
 
-# def test_unweighted_median():
-# 	sample_df = pd.DataFrame(data=[[1,1],[2,1],[3,1]],columns=['val','wt'])
-# 	assert weighted_median(sample_df) == 2
+def test_unweighted_median():
+	sample_df = pd.DataFrame(data=[[1,1],[2,1],[3,1]],columns=['val','wt'])
+	assert weighted_median(sample_df,'val','wt') == 2
